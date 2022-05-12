3de0298ecd10a2b7b69228c1bd4d81159606f73e
Xuân Đặng <dangthanhxuan902@gmail.com>
2022-05-13 00:37:31 +0700

    build foundation scss

---
+++ packages/react/src/index.ts
@@ -0,0 +1,7 @@
+import Color from './atoms/Color';
+import { Spacing } from '@ds.e/foundation';
+
+export {
+    Color,
+    Spacing
+}
---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from'react-dom';
+
+import { Color, Spacing } from '@ds.e/react';
+
+import '@ds.e/scss/lib/Utilities.css';
+
+ReactDOM.render(
+    <Color hexCode='#000' width='lg' height='lg' />,
+    document.getElementById('root')
+)
